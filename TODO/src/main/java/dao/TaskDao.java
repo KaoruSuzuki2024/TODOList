670901e526b2189f4cc8dd30d3e3dfc894e415e9
endokyo <student07@dcom1.onmicrosoft.com>
2024-12-03 14:25:47 +0900

    Merge branch 'main' into test1

--- TODO/src/main/java/dao/TaskDao.java
+++ TODO/src/main/java/dao/TaskDao.java
@@ -1,5 +1,88 @@
 package dao;
 
-public class TaskDao {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
+import 
+
+public class TaskDao {
+	private Connection connection;
+	
+	//コンストラクター
+	//todo_listデータベースとの接続を行う
+	public TaskDao() throws ClassNotFoundException, SQLException{
+		Class.forName("com.mysql.jdbc.Driver");
+		String url = "jdbc:mysql://localhost:3306/java_web_system?useSSL=false";
+		String user = "root";
+		String password = "root";
+		connection = DriverManager.getConnection(url,user,password);
+	}
+	
+	//java_web_systemデータベースと切断するメソッド
+	public void close() {
+		try {
+			if(connection != null) {
+				connection.close();
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
+
+
