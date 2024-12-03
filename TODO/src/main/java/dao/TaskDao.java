54c0d4e1211e9a8a535a42fa1ad98c66dc947326
endokyo <student07@dcom1.onmicrosoft.com>
2024-12-03 17:58:38 +0900

    taskDao作成途中

--- TODO/src/main/java/dao/TaskDao.java
+++ TODO/src/main/java/dao/TaskDao.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
+import java.util.Date;
 
-import 
+import bean.TaskBean;
 
 public class TaskDao {
 	private Connection connection;
@@ -22,6 +22,7 @@ public class TaskDao {
 		connection = DriverManager.getConnection(url,user,password);
 	}
 	
+	
 	//java_web_systemデータベースと切断するメソッド
 	public void close() {
 		try {
@@ -32,12 +33,125 @@ public class TaskDao {
 			e.printStackTrace();
 		}
 	}
+
+
+	//受け取ったTaskBeanを新規登録するメソッド
+	public void insertTask(TaskBean tBean) throws SQLException{
+		TaskBean tb =null;
+		PreparedStatement pstatement = null;
+		
+		try {
+			//SQLを保持するPreparedStatementオブジェクトの生成
+			String sql = "INSERT INTO todo_list.tasks(use_id,task_title,task_deadline,task_priority,task_content,task_check) values(?,?,?,?,?,?);";
+			pstatement = connection.prepareStatement(sql);
+			
+			int userid = tb.getUser_id();
+			String title = tb.getTitle();
+			Date dead = tb.getDeadline();
+			int priority = tb.getPriority();
+			String content = tb.getContent();
+			boolean check = tb.isCheck();
+			
+			pstatement.setInt(1, userid);
+			pstatement.setString(2,title);
+			pstatement.setDate(3,(java.sql.Date) dead);
+			pstatement.setInt(4, priority);
+			pstatement.setString(5,content);
+			pstatement.setBoolean(6, check);
+			
+			//INSERTを実行
+			pstatement.executeUpdate();
+
+		}finally {
+			//PreparedStatementオブジェクトの解放
+			pstatement.close();
+		}	
+	}
 	
 	
+	public void updateTask(TaskBean tBean)throws SQLException {
+		TaskBean tb =null;
+		PreparedStatement pstatement = null;
+		
+		try {
+			//SQLを保持するPreparedStatementオブジェクトの生成
+			String sql = "UPDATE todo_list.tasks SET use_id = ?,task_title = ?,task_deadline = ?,task_priority = ?,task_content = ?,task_check = ?;";
+			pstatement = connection.prepareStatement(sql);
+			
+			int userid = tb.getUser_id();
+			String title = tb.getTitle();
+			Date dead = tb.getDeadline();
+			int priority = tb.getPriority();
+			String content = tb.getContent();
+			boolean check = tb.isCheck();
+			
+			pstatement.setInt(1, userid);
+			pstatement.setString(2,title);
+			pstatement.setDate(3,(java.sql.Date) dead);
+			pstatement.setInt(4, priority);
+			pstatement.setString(5,content);
+			pstatement.setBoolean(6, check);
+			
+			//UPDATEを実行
+			pstatement.executeUpdate();
+			
+		}finally {
+			//PreparedStatementオブジェクトの解放
+			pstatement.close();
+		}	
+	}
 	
 	
+	public void deleteTask(TaskBean tBean)throws SQLException {
+		TaskBean tb =null;
+		PreparedStatement pstatement = null;
+		
+		try {
+			//SQLを保持するPreparedStatementオブジェクトの生成
+			String sql = "DELETE FROM todo_list.tasks WHERE use_id = ?,task_id = ?;";
+			pstatement = connection.prepareStatement(sql);
+			
+			int userid = tb.getUser_id();
+			int taskid = tb.getTask_id();
+			
+			pstatement.setInt(1, userid);
+			pstatement.setInt(2, taskid);
+			
+			//DELETEを実行
+			pstatement.executeUpdate();
+			
+		}finally {
+			//PreparedStatementオブジェクトの解放
+			pstatement.close();
+		}
+	}
 	
 	
+	public TaskBean searchTask(String id) {
+		TaskBean tb = null;
+		PreparedStatement pstatement = null;
+		ResultSet rs = null;
+		try {
+			//SQLを保持するPreparedStatementオブジェクトの生成
+			String sql = "SELECT * FROM tasks WHERE user_id = ?,task_id = ?;";
+			pstatement = connection.prepareStatement(sql);
+			
+			//INパラメータの設定
+			int userid = tb.getUser_id();
+			int taskid = tb.getTask_id();
+			
+			pstatement.setInt(1, userid);
+			pstatement.setInt(2, taskid);
+			
+			//SQLを発行し、抽出結果が格納されたResultSetオブジェクトを取得
+			rs = pstatement.executeQuery();
+			if(rs.next()) {
+				//列名を指定してResultSetオブジェクトから値を取得
+				tb = new TaskBean();
+				//tb.setUser_id();
+			}
+		}
+	}
 	
 	
 	
