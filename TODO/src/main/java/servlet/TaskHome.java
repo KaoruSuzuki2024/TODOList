e1d6ab67df71553c634e1b1c640152c3116b8126
dcom <dcom@2017HP-18>
2024-12-04 17:31:14 +0900

    ホーム画面変更

--- TODO/src/main/java/servlet/TaskHome.java
+++ TODO/src/main/java/servlet/TaskHome.java
@@ -1,6 +1,8 @@
 package servlet;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
@@ -13,6 +15,8 @@ import javax.servlet.http.HttpSession;
 
 import bean.UsersBean;
 import service.CreatUnachievedDead;
+import service.CreatUnachievedPriority;
+import service.SearchTask;
 
 
 /**
@@ -57,12 +61,56 @@ public class TaskHome extends HttpServlet {
 		String btn = request.getParameter("btn");
 		String sort = "dead";
 		String jsp;
-		if(btn != null && !btn.isEmpty()) {
-			if(btn.equals("logout")) {
-				
+		try {
+			if(btn != null && !btn.isEmpty()) {
+				if(btn.equals("logout")) {
+					request.setAttribute("message", "ログアウトしますか？");
+					jsp = "/check.jsp";
+				}else if(btn.equals("regist")) {
+					Date date = new Date(); // ここでDate型の変数を取得
+		        	SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		        	String formattedDate = formatter.format(date);
+		        	request.setAttribute("date", formattedDate);
+		        	jsp = "/TaskEdit.jsp";
+				}else if(btn.equals("sort")) {
+					String nowsort = request.getParameter("nowsort");
+					if(nowsort.equals("dead")) {
+						CreatUnachievedPriority creatlist = new CreatUnachievedPriority();
+						creatlist.execute(request);
+						sort = "priority";
+					}else if(nowsort.equals("priority")) {
+						CreatUnachievedDead search = new CreatUnachievedDead();
+						search.execute(request);
+						sort = "dead";
+					}
+					jsp = "/taskhome.jsp";
+				}else if(btn.equals("delete")) {
+					String taskid = request.getParameter("taskid");
+					request.setAttribute("message","削除してもよいですか");
+					request.setAttribute("deleteid", taskid);
+					jsp = "/check.jsp";
+				}else if(btn.equals("update")) {
+					SearchTask search = new SearchTask();
+					search.execute(request);
+		        	jsp = "/TaskEdit.jsp";
+				}else {
+					jsp = "taskhome.jsp";
+				}
+			}else {
+				request.setAttribute("message", "ボタンが押されませんでした");
+				jsp = "/error.jsp";
 			}
+		}catch (Exception e){
+			e.printStackTrace();
+			request.setAttribute("message", "エラーが発生しました");
+			jsp = "/error.jsp";
 		}
 		
+		request.setAttribute("sort", sort);
+		
+		ServletContext context = getServletContext();
+		RequestDispatcher rd = context.getRequestDispatcher(jsp);
+		rd.forward(request, response);
 	}
 
 }
